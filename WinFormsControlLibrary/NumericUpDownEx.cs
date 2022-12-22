e632adcb759ad0fd21c235675b4f88464caa5c5f
master <master@email.com>
2022-12-22 17:49:47 +0900

    add comment

--- WinFormsControlLibrary/NumericUpDownEx.cs
+++ WinFormsControlLibrary/NumericUpDownEx.cs
@@ -5,19 +5,33 @@ namespace WinFormsControlLibrary
 {
     public partial class NumericUpDownEx : NumericUpDown
     {
+        /// <summary>
+        /// NumericUpDown拡張クラス
+        /// </summary>
         public NumericUpDownEx()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// UpDownボタン押下時のイベント
+        /// </summary>
+        [Category("Action")]
+        [Description("UpDownボタンクリック時に発生します。")]
         public event EventHandler UpDownButtonClicked;
 
+        /// <summary>
+        /// Upボタン押下
+        /// </summary>
         public override void UpButton()
         {
             base.UpButton();
             UpDownButtonClicked?.Invoke(this, EventArgs.Empty);
         }
 
+        /// <summary>
+        /// Downボタン押下
+        /// </summary>
         public override void DownButton()
         {
             base.DownButton();
